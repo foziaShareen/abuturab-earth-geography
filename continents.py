a8428aafc8926ab0f4fabaa55fa960c9e6aff6c5
foziaShareen <foziashareen1@gmail.com>
2026-01-21 15:52:14 +0500

    completed

--- continents.py
+++ continents.py
@@ -1,112 +1,114 @@
-from fastapi import  Body, FastAPI     
-app = FastAPI()
+# from fastapi import  Body, FastAPI     
+# app = FastAPI()
 
-countries = [
-    {
-        "country": "Nigeria",
-        "continent": "Africa"
-    },
-    {
-        "country": "Ghana",
-        "continent": "Africa"
-    },
-    {
-        "country": "Togo",
-        "continent": "Africa"
-    },
-    {
-        "country": "Kenya",
-        "continent": "Africa"
-    },
-    {
-        "country": "Egypt",
-        "continent": "Africa"
-    },
-    {
-        "country": "South Africa",
-        "continent": "Africa"
-    },
-    {
-        "country": "France",
-        "continent": "Europe"
-    },
-    {
-        "country": "Germany",
-        "continent": "Europe"
-    },
-    {
-        "country": "Italy",
-        "continent": "Europe"
-    },
-    {
-        "country": "Spain",
-        "continent": "Europe"
-    },
-    {
-        "country": "Portugal",
-        "continent": "Europe"
-    },
-    {
-        "country": "Netherlands",
-        "continent": "Europe"
-    },
-    {
-        "country": "Belgium",
-        "continent": "Europe"
-    }
-]
-
-
+# countries = [
+#     {
+#         "country": "Nigeria",
+#         "continent": "Africa"
+#     },
+#     {
+#         "country": "Ghana",
+#         "continent": "Africa"
+#     },
+#     {
+#         "country": "Togo",
+#         "continent": "Africa"
+#     },
+#     {
+#         "country": "Kenya",
+#         "continent": "Africa"
+#     },
+#     {
+#         "country": "Egypt",
+#         "continent": "Africa"
+#     },
+#     {
+#         "country": "South Africa",
+#         "continent": "Africa"
+#     },
+#     {
+#         "country": "France",
+#         "continent": "Europe"
+#     },
+#     {
+#         "country": "Germany",
+#         "continent": "Europe"
+#     },
+#     {
+#         "country": "Italy",
+#         "continent": "Europe"
+#     },
+#     {
+#         "country": "Spain",
+#         "continent": "Europe"
+#     },
+#     {
+#         "country": "Portugal",
+#         "continent": "Europe"
+#     },
+#     {
+#         "country": "Netherlands",
+#         "continent": "Europe"
+#     },
+#     {
+#         "country": "Belgium",
+#         "continent": "Europe"
+#     }
+# ]
 
 
 
-@app.get("/countries")
-async def get_countries():
-    return countries
 
-@app.get("/countries/{dynamic_params}")  
-async def get_countries_by_name(dynamic_params: str):
-    for country in countries:
-        if country.get("country").casefold() == dynamic_params.casefold():
-            return country
-    return {"message": "Country not found"}    
+
+# @app.get("/countries")
+# async def get_countries():
+#     return countries
+
+# @app.get("/countries/{dynamic_params}")  
+# async def get_countries_by_name(dynamic_params: str):
+#     for country in countries:
+#         if country.get("country").casefold() == dynamic_params.casefold():
+#             return country
+#     return {"message": "Country not found"}    
     
-@app.get("/countries/")
-async def get_country_continents(country_name: str):
-    countries_to_return = []
-    for country in countries:
-        if country.get("country").casefold() == country_name.casefold():
-            countries_to_return.append(country)
-    return countries_to_return
+# @app.get("/countries/")
+# async def get_country_continents(country_name: str):
+#     countries_to_return = []
+#     for country in countries:
+#         if country.get("country").casefold() == country_name.casefold():
+#             countries_to_return.append(country)
+#     return countries_to_return
 
-@app.get("/countries/{continent_name}/")
-async def get_continent_countries(continent_name: str, country_name: str ):
-    countries_to_return = []
-    for country in countries:
-        if (country.get("continent").casefold() == continent_name.casefold() and
-            country.get("country").casefold() == country_name .casefold()):
-            countries_to_return.append(country)
-    return countries_to_return
+# @app.get("/countries/{continent_name}/")
+# async def get_continent_countries(continent_name: str, country_name: str ):
+#     countries_to_return = []
+#     for country in countries:
+#         if (country.get("continent").casefold() == continent_name.casefold() and
+#             country.get("country").casefold() == country_name .casefold()):
+#             countries_to_return.append(country)
+#     return countries_to_return
 
-@app.post("/countries/create_country")
-async def create_country(new_country=Body()):
-    countries.append(new_country)
+# @app.post("/countries/create_country")
+# async def create_country(new_country=Body()):
+#     countries.append(new_country)
         
         
         
-@app.put("/countries/update_country")
-async def update_country( updated_country=Body()): 
-    for i in range(len(countries)):
-        if countries[i].get("country").casefold() == updated_country.get("country").casefold():
-            countries[i] = updated_country
-            return {"message": "Country updated successfully"}       
+# @app.put("/countries/update_country")
+# async def update_country( updated_country=Body()): 
+#     for i in range(len(countries)):
+#         if countries[i].get("country").casefold() == updated_country.get("country").casefold():
+#             countries[i] = updated_country
+#             return {"message": "Country updated successfully"}       
             
             
-@app.delete("/countries/delete_country/{country_name}")
-async def delete_country(country_name: str):
-    for i in range(len(countries)):
-        if countries[i].get("country").casefold() == country_name.casefold():
-            countries.pop(i)
-            break
-    return {"message": "Country deleted successfully"}
-        
+# @app.delete("/countries/delete_country/{country_name}")
+# async def delete_country(country_name: str):
+#     for i in range(len(countries)):
+#         if countries[i].get("country").casefold() == country_name.casefold():
+#             countries.pop(i)
+#             break
+#     return {"message": "Country deleted successfully"}
+name='weekend'
+mask=len(name)*'*'
+print(f'Today is {mask}')
--- countries2.py
+++ countries2.py
@@ -1,31 +1,89 @@
 from fastapi import FastAPI 
-from fastapi import Body
+from fastapi import Body ,Path,HTTPException
+from pydantic import BaseModel, Field
+from typing import Optional
+from starlette import status 
 
 app = FastAPI()
+
 class Countries:
+    id: int
     country: str
     continent: str
     
-    def __init__(self, country, continent):
+    def __init__(self, id, country, continent):
+        self.id = id
         self.country = country
         self.continent = continent  
+        
+class CountriesModel(BaseModel):
+    id: Optional[int] = Field(default=None, description="ID of the country not required for creation")
+    country: str = Field(min_length=2, max_length=50)
+    continent: str = Field(min_length=2, max_length=50)  
+    
+
+    
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "country": "Brazil",
+                "continent": "South America"
+            }
+        }
+    }
+
+COUNTRIES = [
+    Countries(1, "USA", "North America"),
+    Countries(2, "Canada", "North America"),
+]  
+    
             
-countries=[
-    Countries("Nigeria", "Africa"),
-    Countries("Ghana", "Africa"),
-    Countries("Togo", "Africa"),
-    Countries("Kenya", "Africa"),    
-    Countries("Egypt", "Africa"),
-    Countries("South Africa", "Africa"),
-    Countries("France", "Europe"),
-    Countries("Germany", "Europe"),
-]
+
 
 @app.get("/countries2")
 async def read_countries2():
-    return countries
+    return COUNTRIES
+
+@app.get("/countries2/{country_id}", status_code=status.HTTP_200_OK)
+async def read_country2(country_id: int =Path(gt=0)):
+    for country in COUNTRIES:
+        if country.id == country_id:
+            return country
+    raise HTTPException(status_code=404, detail="Country not found")    
+        
+@app.put("/countries2/update_countries2",status_code=status.HTTP_204_NO_CONTENT)
+async def update_country2(updated_country: CountriesModel):
+    country_changed = False
+    for i in range(len(COUNTRIES)):
+        if COUNTRIES[i].id == updated_country.id:
+            COUNTRIES[i].country = updated_country.country
+            COUNTRIES[i].continent = updated_country.continent
+            country_changed = True
+            return {"message": "Country updated successfully"}
+    if not country_changed:
+        raise HTTPException(status_code=404, detail="Country not found")                    
 
 @app.post("/create_countries2")
-async def create_country2(new_country=Body()):
-    countries.append(new_country)
+async def create_country2(new_country: CountriesModel):
+    new_country = Countries(**new_country.model_dump())
+    print(type(new_country))
+    COUNTRIES.append(find_country_id(new_country))
     return {"message": "Country added successfully"}
+def find_country_id(country :Countries):
+    # if len(COUNTRIES) > 0:
+    #     country.id=COUNTRIES[-1].id + 1
+    # else:
+    #     country.id=1
+    country.id=1 if len(COUNTRIES) == 0 else COUNTRIES[-1].id + 1
+    return country
+
+@app.delete("/countries2/{country_id}")
+async def delete_country2(country_id: int ):
+    country_changed = False
+    for i in range(len(COUNTRIES)):
+        if COUNTRIES[i].id == country_id:
+            COUNTRIES.pop(i)
+            country_changed = True
+    if not country_changed:
+        raise HTTPException(status_code=404, detail="Country not found")
